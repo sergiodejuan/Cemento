3fdb1ef415e9c71fc1783b9753f2c82ef2ab3720
sergiodejuan <sergiodjgm@gmail.com>
2026-03-29 13:02:31 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,5 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  // Simplemente dejamos que la petición continúe sin hacer nada
   return NextResponse.next()
 }
